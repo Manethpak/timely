38c53f01b7dee1f730baedfc849de0a22b260446
Maneth Pak <manethpak00@gmail.com>
2021-12-25 02:19:39 +0700

    add layouts

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <navigation-bar />
+    <notifications position="top left" />
+    <nuxt class="container mx-auto px-6 sm:px-8 lg:px-12" />
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
