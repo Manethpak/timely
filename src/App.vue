8e01183e0e5628d273419716bdf0b40813809b7d
Maneth Pak <manethpak00@gmail.com>
2022-05-29 16:37:37 +0700

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -21,7 +18,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </template>
 
 <style>
-@import '@/assets/base.css';
+@import "@/assets/base.css";
 
 #app {
   max-width: 1280px;
